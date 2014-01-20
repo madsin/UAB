3bdae5831543c52e17dd68e44943b79f9cde7325
Maximilian Fricke <mfricke2808@gmail.com>
2014-01-20 13:22:59 +0100

    Start MPI matrix multiplication; not compiling right now

--- Computational Thinking/MPI/mpi.cpp
+++ Computational Thinking/MPI/mpi.cpp
@@ -1,6 +1,9 @@
 #include <mpi.h>
+#include <stdlib.h>
 #include <iostream>
-#include <string>
+#include <time.h>
+
+#define SIZE 32
 
 int main ( int argc, char **argv ) {
     int retVal;
@@ -15,7 +18,7 @@ int main ( int argc, char **argv ) {
 
     MPI_Comm_size ( MPI_COMM_WORLD, &numTasks );
     MPI_Comm_rank ( MPI_COMM_WORLD, &rank );
-    std::cout << "Hello from Process " << rank << " of " << numTasks << "!" << std::endl;
+    if ( rank == 0 ) std::cout << numTasks << "Processes" << std::endl;
 
     /* Minimum 2 Processes */
     if ( numTasks < 2 ) {
@@ -24,22 +27,78 @@ int main ( int argc, char **argv ) {
         return -1;
     }
 
+    /* Initialize matrices */
     if ( rank == 0 ) {
-        std::string msg = "Hello from process 0!";
-        int tag = 0;
+        double *A, *Bt, *C;
 
-        for ( int dest = 1; dest < numTasks; dest++ ) {
-            retVal = MPI_Send ( (void *) buf, msg.size(), MPI_CHAR, dest, tag, MPI_COMM_WORLD );
+        A  = (double *) malloc ( SIZE * SIZE * sizeof(double) );
+        Bt = (double *) malloc ( SIZE * SIZE * sizeof(double) );
+        C  = (double *) malloc ( SIZE * SIZE * sizeof(double) );
+
+        srand ( time(NULL) );
+
+        for ( int i = 0; i < SIZE; i++ ) {
+        	for ( int j = 0; j < SIZE; j++ ) {
+        		/* A, B random double in [0.5, 1.5] */
+        		A [i*SIZE + j] = 0.5 + ( (double) rand() / RAND_MAX );
+        		Bt[i*SIZE + j] = 0.5 + ( (double) rand() / RAND_MAX );
+        		C [i*SIZE + j] = 0;
+        	}
         }
-    }
 
-    if ( rank != 0 ) {
-    	char buf[256];
-    	// TODO
+        /* Scatter the data */
+        /* Synchronous data divison */
+        if ( !( SIZE % numTasks) ) {
+        	int stepSize = SIZE / numTasks;
+        	MPI_Request reqsA[stepSize * stepSize];
+        	MPI_Request reqsB[stepSize * stepSize];
+        	int itDest = 0;
+        	int tag = 0;
+
+        	/* Data can be divided into submatrices perfectly */
+        	/* First submatrix is calculated by root process itself */
+			for ( int itRow = stepSize; itRow < SIZE; itRow+=stepSize ) {
+				for ( int itCol = stepSize; itCol < SIZE; itCol+=stepSize ) {
+					/* Nonblocking send */
+					itDest++;
+
+					MPI_Isend ( (void *) &A[itRow * SIZE],
+							    stepSize * SIZE,
+							    MPI_DOUBLE,
+							    itDest,
+							    tag,
+							    MPI_COMM_WORLD,
+							    &reqsA[itDest] );
+
+					MPI_Isend ( (void *) &Bt[itCol * SIZE],
+								stepSize * SIZE,
+								MPI_DOUBLE,
+								itDest,
+								tag,
+								MPI_COMM_WORLD,
+								&reqsB[itDest] );
+				}
+			}
+
+		/* Asynchronous data divsion */
+        } else {
+        	/* TODO: Asynchronous data divison */
+        	std::cout << "Asynchronous data divison not yet implemented" << std::endl;
+        	return -1;
+        }
+
+        /* Do own calculation */
+
+        /* Gather the data */
+
+        /* Print results */
+
+        /* Free memory */
+        free(A); free(Bt); free(C);
     }
 
     /* Finalize MPI task */
     retVal = MPI_Finalize();
 
-	return 0;
+    return 0;
 }

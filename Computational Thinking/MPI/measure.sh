2261ae7dd2c53425b8b10180f3cafcfab9defbae
Maximilian Fricke <mfricke2808@gmail.com>
2014-01-22 20:21:50 +0100

    Added script for various MPI measurements

---
+++ Computational Thinking/MPI/measure.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+export MPI_DIR="/home/master/master29/UAB/Computational Thinking/MPI"
+
+# Matrix multiplication
+for DIM in 512 1024 2048
+do
+    for N_TASKS in 2 4 8 16 32
+    do
+        CMD="bash $MPI_DIR/matmul/run.sh $N_TASKS $DIM $DIM"
+        echo "Executing $CMD"
+        #echo $(CMD)
+    done
+done
+
+# Jacobi
+for C in 1 2 4
+do
+    for N_TASKS in 2 4 8 16 32
+    do
+        CMD="bash $MPI_DIR/jacobi/run.sh $N_TASKS $C"
+        echo "Executing $CMD"
+        TEST="ls"
+        #echo $(CMD)
+    done
+done

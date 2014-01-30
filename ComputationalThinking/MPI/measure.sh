a0a0796e2803b9ff694d6b1554396a26960a5354
Maximilian Fricke <max.steaky@web.de>
2014-01-30 20:08:42 +0100

    Fixes in measure.sh

--- ComputationalThinking/MPI/measure.sh
+++ ComputationalThinking/MPI/measure.sh
@@ -15,10 +15,10 @@ echo "# Matrix Multiplication #"
 echo "#########################"
 
 # For quadratic no. of tasks betwen 10..80
-for N_TASKS in 4 9 16 25 36 49 64
+for N_TASKS in 9 16 25 36
 do
     # Assume dimM = dimO = dimN
-    for DIM in 1024 2048 4096 8192
+    for DIM in 1680 3360 5040 6720 8400
     do
         # Slow Matmul
         cd ${MPI_DIR}/matmul/
@@ -28,8 +28,8 @@ do
         echo "#########################"
 
         # Fast Matmul
-        cd ${MPI_DIR}/matmul/
-        QSUB="${QSUB0} -N matmul_slow_${N_TASKS}_${DIM}.log -pe mpi-RR ${N_TASKS} run.sh ${N_TASKS} ${DIM} ${DIM}"
+        cd ${MPI_DIR}/project/
+        QSUB="${QSUB0} -N matmul_fast_${N_TASKS}_${DIM}.log -pe mpi-RR ${N_TASKS} run.sh ${N_TASKS} ${DIM} ${DIM} ${DIM}"
         echo $QSUB
         echo $($QSUB)
         echo "#########################"

5d19a9f2e5d7289befa2fc2fbd876809bedf3130
Maximilian Fricke <mfricke2808@gmail.com>
2014-02-09 18:40:31 +0100

    Beautifying debug output

--- ComputationalThinking/MPI/jacobi/jacobi.cpp
+++ ComputationalThinking/MPI/jacobi/jacobi.cpp
@@ -9,15 +9,20 @@
 #define PRECISION (1.0e-6)
 
 void printMat ( double *M, int size ) {
+    if ( size > 16 ) return;
+
 	for ( int i = 0; i < size; i++ ) {
 		for ( int j = 0; j < size; j++ ) {
 			std::cout << M[i*size + j] << " ";
 		}
 		std::cout << std::endl;
 	}
+    std::cout << std::endl;
 }
 
 void printVec ( double *vec, int size ) {
+    if ( size > 16 ) return;
+
 	for ( int i = 0; i < size; i++ ) {
 		std::cout << vec[i] << " ";
 	}
@@ -30,6 +35,7 @@ int main ( int argc, char **argv ) {
     int itCount = 0;
     int dim, c;
     double times[4], start, stop, calcTime = 0;
+    double storeOldXTime = 0, bcastXTime = 0, gatherXNewTime = 0;
     double epsilon = 1;
     double *A, *b, *x, *x_old, *myA, *myB, *myX_new;
 
@@ -96,7 +102,7 @@ int main ( int argc, char **argv ) {
         			return -1;
         		}
         	}
-        std::cout << "Created diagonal dominant matrix A=" << std::endl;
+        std::cout << "Created diagonal dominant matrix A" << std::endl;
         printMat( A, dim );
         }
 
@@ -109,7 +115,7 @@ int main ( int argc, char **argv ) {
         	// Initial solution
         	x[i] = 0.5 + ( (double) rand() / RAND_MAX );
         }
-        if (DEBUG) {
+        if (DEBUG && (dim <= 16)) {
         	std::cout << "b =" << std::endl;
         	printVec ( b, dim );
         	std::cout << "x_0 =" << std::endl;
@@ -136,9 +142,11 @@ int main ( int argc, char **argv ) {
     if (DEBUG) {
     	stop = MPI_Wtime();
     	std::cout << "P" << rank << ": MPI_Scatter(A) took " << stop-start << "s" << std::endl;
-    	std::cout << "P" << rank << ": myA=" << std::endl;
-    	for ( int i = 0; i < stepSize; i++ ) {
-    		printVec ( &myA[i*dim], dim );
+    	if (dim <= 16 ) {
+    		std::cout << "P" << rank << ": myA=" << std::endl;
+    		for ( int i = 0; i < stepSize; i++ ) {
+    			printVec ( &myA[i*dim], dim );
+    		}
     	}
     }
     // b
@@ -149,45 +157,45 @@ int main ( int argc, char **argv ) {
     if (DEBUG) {
     	stop = MPI_Wtime();
 		std::cout << "P" << rank << ": MPI_Scatter(b) took " << stop-start << "s" << std::endl;
-    	std::cout << "P" << rank << ": myB=" << std::endl;
-    	for ( int i = 0; i < stepSize; i++ ) {
-    		std::cout << myB[i] << " ";
+    	if (dim <= 16) {
+    		std::cout << "P" << rank << ": myB=" << std::endl;
+    		for ( int i = 0; i < stepSize; i++ ) {
+    			std::cout << myB[i] << " ";
+    		}
+    		std::cout << std::endl;
     	}
-    	std::cout << std::endl;
     }
 
     /* Iterate */
     if (DEBUG) std::cout << "P" << rank << ": Iterate()" << std::endl;
     while ( epsilon > PRECISION ) {
     	itCount++;
-    	if (DEBUG) {
+    	if (DEBUG && (dim <= 16)) {
     		std::cout << "P" << rank << ": Starting iteration " << itCount << std::endl;
     	}
 
+    	if (DEBUG) start = MPI_Wtime();
     	/* Store old x */
     	if ( rank == 0 ) {
     		for ( int i = 0; i < dim; i++ ) {
     			x_old[i] = x[i];
     		}
     	}
+    	if (DEBUG) storeOldXTime += MPI_Wtime() - start;
 
     	/* Incremental synchronous data distribution */
     	if (DEBUG) start = MPI_Wtime();
     	MPI_Bcast ( x, dim, MPI_DOUBLE, 0, MPI_COMM_WORLD );
-    	if ( (rank == 0) && (DEBUG) ) {
+    	if ( (rank == 0) && (DEBUG) && (dim <= 16)) {
     		std::cout << "x_" << itCount-1 << "=" << std::endl;
     		printVec ( x, dim );
     	}
-    	if (DEBUG) {
-    		stop = MPI_Wtime();
-			std::cout << "P" << rank << ": MPI_Bcast(x) took " << stop-start << "s" << std::endl;
-    	}
+    	if (DEBUG) bcastXTime += MPI_Wtime() - start;
 
     	/* Take time */
     	times[1] = MPI_Wtime();
 
     	/* Calculate */
-    	if (DEBUG) std::cout << "P" << rank << ": Calculate()" << std::endl;
     	for ( int i = 0; i < stepSize; i++ ) {
     		myX_new[i] = myB[i];
     		for ( int j = 0; j < dim; j++ ) {
@@ -196,7 +204,7 @@ int main ( int argc, char **argv ) {
     			myX_new[i] -= myA[i*dim + j] * x[j];
     		}
     		myX_new[i] /= myA[i*dim + i + rank*stepSize];
-    		if (DEBUG) std::cout << "P" << rank << ": myX_new[" << i << "]="
+    		if (DEBUG && (dim <= 16)) std::cout << "P" << rank << ": myX_new[" << i << "]="
     				             << myX_new[i] << std::endl;
     	}
 
@@ -209,19 +217,14 @@ int main ( int argc, char **argv ) {
     	MPI_Gather( myX_new, stepSize, MPI_DOUBLE,
     			    x, stepSize, MPI_DOUBLE,
     			    0, MPI_COMM_WORLD );
-    	if (DEBUG) {
-    		stop = MPI_Wtime();
-			std::cout << "P" << rank << ": MPI_Gather(x) took " << stop-start << "s" << std::endl;
-    	}
-    	if ( (DEBUG) && (rank == 0) ) {
+    	if (DEBUG) gatherXNewTime += MPI_Wtime() - start;
+    	if ( (DEBUG) && (rank == 0) && (dim <= 16) ) {
     		std::cout << "x_" << itCount << " =" << std::endl;
     		printVec ( x, dim );
     	}
 
     	/* Calculate epsilon */
     	if ( rank == 0 ) {
-    		if (DEBUG) std::cout << "P" << rank << ": epsilon()" << std::endl;
-
     		/* Take time */
     		times[1] = MPI_Wtime();
 
@@ -235,9 +238,8 @@ int main ( int argc, char **argv ) {
     		times[2] = MPI_Wtime();
     		calcTime += times[2]-times[1];
     	}
-    	if (DEBUG) {
+    	if (DEBUG && (dim <= 16)) {
     		if (rank == 0) std::cout << "epsilon_" << itCount << " = " << epsilon << std::endl;
-    		std::cout << "P" << rank << ": MPI_Bcast(epsilon)" << std::endl;
     	}
     	MPI_Bcast ( &epsilon, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD );
     }
@@ -250,8 +252,11 @@ int main ( int argc, char **argv ) {
 
     /* Print results */
     if ( rank == 0 ) std::cout << "Total of " << itCount << " iterations performed" << std::endl;
-    std::cout << "P" << rank << ": Execution time = " << (times[1] - times[0]) << std::endl
-    		                 << "Calculation time = " << calcTime << std::endl;
+    std::cout << "P" << rank << ": Execution time = " << (times[1] - times[0]) << ", "
+    		  << "Calculation time = " << calcTime << ", "
+    		  << "Time to store old x = " << storeOldXTime << ", "
+    		  << "Time to broadcast x = " << bcastXTime << ", "
+    		  << "Time to gather x = " << gatherXNewTime << std::endl;
 
     /* Free Memory */
     if ( rank == 0) {
